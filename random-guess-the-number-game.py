a722da970bdce822a53799964e2973499a01963a
subpanda101 <pokemonbrandon1234@gmail.com>
2016-11-15 20:07:55 +0000

    Added the restart code

---
+++ random-guess-the-number-game.py
@@ -0,0 +1,36 @@
+import random
+import sys
+import os
+
+def restart_program():
+    python = sys.executable
+    os.execl(python, python, * sys.argv);
+
+def clear():
+    os.system('cls' if os.name=='nt' else 'clear');
+
+name = input ("Hello, What is your name?");
+print("Welcome" , name , ", try and guess a number between 1 and 20");
+
+num = (random.randint (1,20));
+
+while True:
+
+    guess = int(input ("Your Guess: "));
+
+    if guess < num :
+        print("Too low: ");
+
+    if guess > num :
+         print("Too high: ");
+
+    if guess == num :
+        print("You've guessed it! It's" , num);
+        break;
+
+restart = input("Do (y)ou want to play again? ");
+if restart == "y":
+    clear();
+    restart_program();
+else:
+    sys.exit();
